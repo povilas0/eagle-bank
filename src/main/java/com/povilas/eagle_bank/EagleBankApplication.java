5bbc01e961f2e0f2dee4c28d0efeaf1853bd3d3f
povilas <povilas.d@gmail.com>
2026-03-21 21:02:57 +0000

    feat: Initial commit

---
+++ src/main/java/com/povilas/eagle_bank/EagleBankApplication.java
@@ -0,0 +1,13 @@
+package com.povilas.eagle_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleBankApplication.class, args);
+	}
+
+}
