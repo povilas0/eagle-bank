7233019ed6dc1bec90bda4eb91afbf6956aeeaa3
povilas <povilas.d@gmail.com>
2026-03-21 23:06:06 +0000

    fix: remove public keyword from main method

--- src/main/java/com/povilas/eagle_bank/EagleBankApplication.java
+++ src/main/java/com/povilas/eagle_bank/EagleBankApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EagleBankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EagleBankApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(EagleBankApplication.class, args);
+    }
 
 }
